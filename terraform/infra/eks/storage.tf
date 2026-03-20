c2904858ef4d73e9fdfebcd9e59033054217eb95
rusXL <ruslan.melnyk.x@gmail.com>
2026-03-20 11:36:05 +0100

    dump

--- terraform/infra/eks/storage.tf
+++ terraform/infra/eks/storage.tf
@@ -22,3 +22,47 @@ resource "aws_launch_template" "eks_nodes" {
     http_put_response_hop_limit = 2
   }
 }
+
+
+# # Pod Identity addon
+# resource "aws_eks_addon" "pod_identity" {
+#   cluster_name = aws_eks_cluster.cluster_a.name
+#   addon_name   = "eks-pod-identity-agent"
+#   depends_on   = [aws_eks_node_group.node_group_a]
+# }
+
+# # IAM role for EBS CSI
+# resource "aws_iam_role" "ebs_csi" {
+#   name = "eks-ebs-csi-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect    = "Allow"
+#       Principal = { Service = "pods.eks.amazonaws.com" }
+#       Action    = ["sts:AssumeRole", "sts:TagSession"]
+#     }]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "ebs_csi" {
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+#   role       = aws_iam_role.ebs_csi.name
+# }
+
+# # Associate role with EBS CSI service account
+# resource "aws_eks_pod_identity_association" "ebs_csi" {
+#   cluster_name    = aws_eks_cluster.cluster_a.name
+#   namespace       = "kube-system"
+#   service_account = "ebs-csi-controller-sa"
+#   role_arn        = aws_iam_role.ebs_csi.arn
+# }
+
+# # EBS CSI addon
+# resource "aws_eks_addon" "ebs_csi" {
+#   cluster_name = aws_eks_cluster.cluster_a.name
+#   addon_name   = "aws-ebs-csi-driver"
+#   depends_on = [
+#     aws_eks_pod_identity_association.ebs_csi,
+#     aws_eks_node_group.node_group_a
+#   ]
+# }
