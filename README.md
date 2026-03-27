36375c20a2998ef2f55a239542c83a6285bfab50
rusXL <ruslan.melnyk.x@gmail.com>
2026-03-27 08:36:00 +0100

    dump

--- README.md
+++ README.md
@@ -7,10 +7,7 @@
 ## Security
 - restrict vpc firewall rules in infra for gcp and aws
 
-## Load
-- fake load service that will allow to test system under load till failure
-
 ## HA
 - pod resource requests, limits, hpa for api, more pods for tidb cluster
 - scheduling policies to distribute components evenly across nodes
-- calculate availability of the system
+- calculate availability of the system with respect to stated SLO
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -16,10 +16,6 @@ LB_IP=$(terraform output -raw gke_lb_ip)
 sed -i "s/\"frontend\.[0-9.]*\.nip\.io\"/\"frontend.${LB_IP}.nip.io\"/" ../../kubernetes/gke/frontend/03-ingress.yaml
 sed -i "s/\"grafana\.[0-9.]*\.nip\.io\"/\"grafana.${LB_IP}.nip.io\"/" ../../kubernetes/gke/grafana/02-ingress.yaml
 
-git add ../../kubernetes/gke/frontend/03-ingress.yaml ../../kubernetes/gke/grafana/02-ingress.yaml
-git commit -m "chore: update LB IP to ${LB_IP} in ingress manifests"
-git push
-
 cd ../platform
 terraform init
 terraform apply -auto-approve
