e93a284da34e3fe664d2b4517f5c3a808c991786
rusXL <ruslan.melnyk.x@gmail.com>
2026-03-28 13:22:38 +0100

    dump

--- api/app/models/item.py
+++ api/app/models/item.py
@@ -7,6 +7,8 @@ from app.schemas.item import ItemState
 class Item(Base):
     __tablename__ = "items"
 
-    id = Column(BigInteger, primary_key=True, autoincrement=False, server_default=FetchedValue())
+    id = Column(
+        BigInteger, primary_key=True, autoincrement=False, server_default=FetchedValue()
+    )
     value = Column(String(255), unique=True, nullable=False)
     state = Column(Integer, default=ItemState.incomplete, nullable=False)
