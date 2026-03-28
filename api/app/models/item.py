b73304d0d7cf3228dcb7ae795eccf89b1331af9f
rusXL <ruslan.melnyk.x@gmail.com>
2026-03-28 13:17:51 +0100

    dump

--- api/app/models/item.py
+++ api/app/models/item.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger, Column, Integer, String
+from sqlalchemy import BigInteger, Column, FetchedValue, Integer, String
 
 from app.database import Base
 from app.schemas.item import ItemState
@@ -7,6 +7,6 @@ from app.schemas.item import ItemState
 class Item(Base):
     __tablename__ = "items"
 
-    id = Column(BigInteger, primary_key=True, autoincrement=False)
+    id = Column(BigInteger, primary_key=True, autoincrement=False, server_default=FetchedValue())
     value = Column(String(255), unique=True, nullable=False)
     state = Column(Integer, default=ItemState.incomplete, nullable=False)
