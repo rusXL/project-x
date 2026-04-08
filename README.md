824229683b5049f272663c5d5c6b9095369e7a59
rusXL <ruslan.melnyk.x@gmail.com>
2026-04-08 18:54:22 +0200

    dump

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 - test firewall rules
 - loadtest target: 1000 RPS + SLO
 - calculate availability of the system with respect to stated SLO
+- k6 pods with loadtest must also be tested on GKE cluster
+- section 6 of thesis
--- terraform/infra/eks/security.tf
+++ terraform/infra/eks/security.tf
@@ -7,5 +7,5 @@ resource "aws_security_group_rule" "allow_http_from_gcp" {
   protocol          = "tcp"
   cidr_blocks       = [var.gcp_vpc_cidr, var.gcp_pod_cidr]
   security_group_id = aws_eks_cluster.cluster_a.vpc_config[0].cluster_security_group_id
-  description       = "HTTP from GCP (frontend -> API, Grafana proxy via Traefik)"
+  description       = "HTTP from GCP (frontend to API, Grafana proxy via Traefik)"
 }
