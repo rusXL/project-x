e9bceb9a8346a415b32ac9e0964ea0bb44960060
rusXL <ruslan.melnyk.x@gmail.com>
2026-04-13 09:53:48 +0200

    dump

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 
 EKS_CTX="arn:aws:eks:us-east-1:454371013564:cluster/cluster-a"
-GKE_CTX="gke_cloud-computing-476715_us-central1-a_cluster-g"
+GKE_CTX="gke_cloud-computing-476715_us-east4-a_cluster-g"
 
 # infra
 cd terraform/infra
@@ -23,13 +23,13 @@ cd ../..
 
 # kubeconfig
 aws eks update-kubeconfig --name cluster-a --region us-east-1
-gcloud container clusters get-credentials cluster-g --zone us-central1-a --project cloud-computing-476715
+gcloud container clusters get-credentials cluster-g --zone us-east4-a --project cloud-computing-476715
 
 # tidb crds + operator
-kubectl apply -f https://github.com/pingcap/tidb-operator/releases/download/v2.0.0/tidb-operator.crds.yaml \
-  --server-side --context "$EKS_CTX"
-kubectl apply -f https://github.com/pingcap/tidb-operator/releases/download/v2.0.0/tidb-operator.yaml \
-  --server-side --context "$EKS_CTX"
+# kubectl apply -f https://github.com/pingcap/tidb-operator/releases/download/v2.0.0/tidb-operator.crds.yaml \
+#   --server-side --context "$EKS_CTX"
+# kubectl apply -f https://github.com/pingcap/tidb-operator/releases/download/v2.0.0/tidb-operator.yaml \
+#   --server-side --context "$EKS_CTX"
 
 # rancher agent on EKS
 MANIFEST_URL=$(cd terraform/platform && terraform output -raw eks_registration_manifest)
